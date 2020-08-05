e0879f8d37cb3c45b71e28bae913a354f022f76f
Raja Rahul Maddula <raja.rahul.maddula@ibm.com>
2020-08-05 17:15:04 +0530

    Login Page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
-import React from 'react';
-import './App.css';
-import Login from "./Login"
+import React from "react";
+import "./App.css";
+import Login from "./Login";
 
 // eb45f7be8025446888a14d1851005ec2
 
 function App() {
   return (
     <div className="app">
-
       {/* Spotify clone */}
       {/* Login with spotify */}
       <Login />
-
     </div>
   );
 }
--- src/Login.js
+++ src/Login.js
@@ -1,14 +1,18 @@
-import React from 'react'
+import React from "react";
+import "./Login.css";
 
 function Login() {
   return (
-    <div>
-      
-      <h1>Login</h1>
+    <div className="login">
       {/* Spotify logo */}
+      <img
+        src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
+        alt="Spotify"
+      />
       {/* Login with spotify */}
+      <a>LOGIN WITH SPOTIFY</a>
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
