f42167b593900bd1c73fa7cfe1031185f535db35
rajarahul12 <maddula.rajarahul@gmail.com>
2020-08-05 22:09:18 +0530

    Adding header

--- src/Player.js
+++ src/Player.js
@@ -11,7 +11,7 @@ function Player({ spotify }) {
         {/* Side Bar */}
         <SideBar />
         {/* Body */}
-        <Body />
+        <Body spotify={spotify} />
       </div>
       {/* Footer */}
       <Footer />
