7c7b49a793ce58bdabcdcdd8e018eb4b04ca51c3
Raja Rahul Maddula <raja.rahul.maddula@ibm.com>
2020-08-05 17:36:30 +0530

    Update spotify.js

--- src/spotify.js
+++ src/spotify.js
@@ -1,5 +1,4 @@
 // https://developer.spotify.com/documentation/web-playback-sdk/quick-start/#
-
 export const authEndpoint = "https://accounts.spotify.com/authorize";
 // Replace with your app's client ID, redirect URI and desired scopes
 
